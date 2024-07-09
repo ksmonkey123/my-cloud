5a0ad4c576c517dcdd299c7340cf59a4a7b830ba
Andreas Wälchli <andreas.waelchli@me.com>
2024-07-09 17:19:19 +0000

    shedlock: print hostname on startup

--- lib-common/src/main/kotlin/ch/awae/mycloud/shedlock/ShedlockConfiguration.kt
+++ lib-common/src/main/kotlin/ch/awae/mycloud/shedlock/ShedlockConfiguration.kt
@@ -1,8 +1,10 @@
 package ch.awae.mycloud.shedlock
 
+import ch.awae.mycloud.*
 import net.javacrumbs.shedlock.core.*
 import net.javacrumbs.shedlock.provider.jdbctemplate.*
 import net.javacrumbs.shedlock.spring.annotation.*
+import net.javacrumbs.shedlock.support.Utils
 import org.springframework.context.annotation.*
 import org.springframework.jdbc.core.*
 import javax.sql.*
@@ -10,6 +12,9 @@ import javax.sql.*
 @Configuration
 @EnableSchedulerLock(defaultLockAtMostFor = "\${shedlock.default-timeout:PT30S}")
 class ShedlockConfiguration {
+
+    val logger = createLogger()
+
     @Bean
     fun lockProvider(dataSource: DataSource): LockProvider {
         return JdbcTemplateLockProvider(
@@ -17,6 +22,8 @@ class ShedlockConfiguration {
                 .withJdbcTemplate(JdbcTemplate(dataSource))
                 .usingDbTime()
                 .build()
-        )
+        ).also {
+            logger.info("Hostname is '${Utils.getHostname()}'")
+        }
     }
 }
