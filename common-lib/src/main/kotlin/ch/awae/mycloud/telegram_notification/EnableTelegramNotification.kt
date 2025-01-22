51d12ec7bc66856778d01440de2b930c9d5a2944
Andreas Wälchli <andreas.waelchli@me.com>
2025-01-22 15:38:11 +0000

    deprecate everything in telegram-notifications

--- common-lib/src/main/kotlin/ch/awae/mycloud/telegram_notification/EnableTelegramNotification.kt
+++ common-lib/src/main/kotlin/ch/awae/mycloud/telegram_notification/EnableTelegramNotification.kt
@@ -3,6 +3,7 @@ package ch.awae.mycloud.telegram_notification
 import ch.awae.mycloud.kafka.*
 import org.springframework.context.annotation.*
 
+@Deprecated("use ch.awae.mycloud.notification.* instead")
 @Target(AnnotationTarget.CLASS)
 @Retention(AnnotationRetention.RUNTIME)
 @MustBeDocumented
--- common-lib/src/main/kotlin/ch/awae/mycloud/telegram_notification/TelegramNotificationService.kt
+++ common-lib/src/main/kotlin/ch/awae/mycloud/telegram_notification/TelegramNotificationService.kt
@@ -3,8 +3,10 @@ package ch.awae.mycloud.telegram_notification
 import org.springframework.kafka.core.*
 import org.springframework.stereotype.*
 
+@Deprecated("use ch.awae.mycloud.notification.* instead")
 const val TELEGRAM_NOTIFICATION_TOPIC = "mycloud.telegram-notification"
 
+@Deprecated("use ch.awae.mycloud.notification.* instead")
 @Service
 class TelegramNotificationService(private val kafka: KafkaTemplate<String, Any>) {
 
