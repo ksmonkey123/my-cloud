fbe316f933a918e0893567664921d9136975ab2a
Andreas Wälchli <andreas.waelchli@me.com>
2025-01-22 15:36:38 +0000

    deprecate markdown format for telegram messages

--- common-lib/src/main/kotlin/ch/awae/mycloud/telegram_notification/TelegramNotificationMessage.kt
+++ common-lib/src/main/kotlin/ch/awae/mycloud/telegram_notification/TelegramNotificationMessage.kt
@@ -1,7 +1,9 @@
 package ch.awae.mycloud.telegram_notification
 
 enum class TelegramMessageFormat {
-    HTML, MARKDOWN
+    HTML,
+    @Deprecated("only html will be supported going forward")
+    MARKDOWN
 }
 
 data class TelegramNotificationMessage(val text: String, val format: TelegramMessageFormat)
