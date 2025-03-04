5f016a2bbc7f8bf30f0dc6ab15930f6de2225957
Andreas Wälchli <andreas.waelchli@me.com>
2025-03-04 15:59:06 +0000

    gui: fix cache breaking translations

--- gui/src/app/transloco-loader.ts
+++ gui/src/app/transloco-loader.ts
@@ -1,12 +1,15 @@
-import { inject, Injectable } from "@angular/core";
-import { Translation, TranslocoLoader } from "@jsverse/transloco";
-import { HttpClient } from "@angular/common/http";
+import {inject, Injectable} from "@angular/core";
+import {Translation, TranslocoLoader} from "@jsverse/transloco";
+import {HttpClient} from "@angular/common/http";
 
-@Injectable({ providedIn: 'root' })
+import checksums from '../../i18n-checksums.json'
+import {LanguageCode} from "./common/language.model";
+
+@Injectable({providedIn: 'root'})
 export class TranslocoHttpLoader implements TranslocoLoader {
-    private http = inject(HttpClient);
+  private http = inject(HttpClient);
 
-    getTranslation(lang: string) {
-        return this.http.get<Translation>(`/assets/i18n/${lang}.json`);
-    }
+  getTranslation(lang: LanguageCode) {
+    return this.http.get<Translation>(`/assets/i18n/${lang}.json?v=${checksums[lang]}`);
+  }
 }
