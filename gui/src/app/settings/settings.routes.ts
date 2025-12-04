0a6594ecb44f7c88639fd37ea945ece4757fb421
Andreas Wälchli <andreas.waelchli@me.com>
2025-12-04 18:11:07 +0000

    #7: frontend cleanup. merge role service into user service

--- gui/src/app/settings/settings.routes.ts
+++ gui/src/app/settings/settings.routes.ts
@@ -1,7 +1,6 @@
 import {Routes} from "@angular/router";
 import {AccountSettingsComponent} from "./account-settings/account-settings.component";
 import {UserManagementService} from "./user-management/user-management.service";
-import {RoleManagementService} from "./role-management/role-management.service";
 import {UserListComponent} from "./user-management/user-list/user-list.component";
 import {UserDetailsComponent} from "./user-management/user-details/user-details.component";
 import {ApiKeyManagementComponent} from "./api-key-management/api-key-management.component";
@@ -13,7 +12,7 @@ export const settings: SettingsInformation = {
     {path: 'account', component: AccountSettingsComponent},
     {path: 'api_keys', component: ApiKeyManagementComponent},
     {
-      path: 'users', providers: [UserManagementService, RoleManagementService], children: [
+      path: 'users', providers: [UserManagementService], children: [
         {path: '', component: UserListComponent},
         {path: ':username', component: UserDetailsComponent},
       ]
@@ -24,7 +23,7 @@ export const settings: SettingsInformation = {
     {path: '/account', titleKey: 'navbar.account', auth: 'user', icon: 'person'},
     {path: '/api_keys', titleKey: 'navbar.api-keys', auth: 'user', icon: 'key'},
     {path: '/users', titleKey: 'navbar.users', auth: 'admin', icon: 'manage_accounts'},
-    {path: '/features',  titleKey: 'navbar.features',  auth: 'admin',  icon: 'rule'},
+    {path: '/features', titleKey: 'navbar.features', auth: 'admin', icon: 'rule'},
   ]
 }
 
