c1001d13899aad66c5847c301d960e80a34687b5
Andreas Wälchli <andreas.waelchli@me.com>
2025-12-11 13:51:04 +0100

    config  tweaks for windows

--- gui/i18n-generate-checksums.js
+++ gui/i18n-generate-checksums.js
@@ -11,7 +11,7 @@ function generateChecksum(str, algorithm, encoding) {
 
 const result = {};
 
-glob.sync(`src/assets/i18n/**/*.json`).forEach(path => {
+glob.sync(`./src/assets/i18n/**/*.json`, { posix: true, dotRelative: true }).forEach(path => {
   const [_, lang] = path.split('src/assets/i18n/');
   const content = fs.readFileSync(path, {encoding: 'utf-8'});
   result[lang.replace('.json', '')] = generateChecksum(content);
