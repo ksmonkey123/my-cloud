8f2e3c9b218f5e102f9f4ee156266b6de0837417
Andreas Wälchli <andreas.waelchli@me.com>
2025-12-12 23:59:19 +0100

    start with api key management gui

--- gui/src/app/settings/api-key-management/api-key-management.component.ts
+++ gui/src/app/settings/api-key-management/api-key-management.component.ts
@@ -1,10 +1,19 @@
 import {Component} from '@angular/core';
 import {ApiKey, ApiKeyManagementService} from "./api-key-management.service";
 import {BaseDataComponent} from "../../common/base/base-data.component";
+import {TranslocoDirective} from "@jsverse/transloco";
+import {MatAccordion, MatExpansionPanel, MatExpansionPanelHeader} from "@angular/material/expansion";
+import {MatIcon} from "@angular/material/icon";
 
 @Component({
   selector: 'app-api-key-management',
-  imports: [],
+  imports: [
+    TranslocoDirective,
+    MatAccordion,
+    MatExpansionPanel,
+    MatExpansionPanelHeader,
+    MatIcon
+  ],
   providers: [ApiKeyManagementService],
   templateUrl: './api-key-management.component.html',
   styleUrl: './api-key-management.component.scss'
